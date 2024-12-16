56edde4dfdb7d108767e14e0f4047c25d72f1d70
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2024-12-16 14:57:19 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,6 @@
 
 cd ~
 cd .update
-wget https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/mesa.sh | bash
+wget https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/mesa.sh
+chmod +x mesa.sh
+bash mesa.sh
