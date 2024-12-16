670e8419512ac31a3a6ec0082e63ac48ad1d7afa
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2024-12-16 14:58:54 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ cd .update
 wget https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/mesa.sh
 chmod +x mesa.sh
 bash mesa.sh
+clear
+printf "mesa foi atualizado \n"
