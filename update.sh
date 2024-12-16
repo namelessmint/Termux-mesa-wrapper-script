8c2ee1aa35ed3d0e07f85888acac25f6717e43d8
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2024-12-16 14:47:55 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,4 @@
 
 cd ~
 cd .update
-wget https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/16%2F12/mesaup.deb
-apt install ./*.deb -y
-rm *.deb
+wget https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/mesa.sh | bash
