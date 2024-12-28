55867d5cdb18534d1ebdeb62fed5d42abda4b45d
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2024-12-28 12:56:10 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash                                          
-cd ~/.update     
-
+cd ~/.update
 clear
 
 
@@ -8,11 +7,11 @@ echo "Escolha a versão do Mesa para instalar:"
 echo "1. Versão para Xclipe e Adreno (24.2.5-12)"
 echo "2. Versão mais recente (24.3.1-3)"
 echo "3. Versão para Mali (24.2.5-9)"
-echo "4. Sair"
-read -p "Digite o número da sua escolha: " escolha
+echo "4. Cancelar instalação"
+read -p "Digite o número da sua escolha: " versao
 
-URL_ATUAL="https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/16%2F12/mesaup.deb"
-URL_RECENTE="https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/Mesamali/mesa-vulkan-icd-wrapper_24.3.1-3_aarch64.deb"
+URL_XCLIPSE="https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/16%2F12/mesaup.deb"
+URL_BE="https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/Mesamali/mesa-vulkan-icd-wrapper_24.3.1-3_aarch64.deb"
 URL_MALI="https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/Mesamali/mesa.deb"
 
 instalar_mesa() {
@@ -23,18 +22,18 @@ instalar_mesa() {
     chmod +x $arquivo
 
     if [[ $arquivo == *.deb ]]; then
-        echo "Instalando pacote o mesa..."
+        echo "Instalando o mesa..."
         dpkg -i $arquivo
         rm *.deb
     fi
 }
 
-case $escolha in
+case $versao in
     1)
-        instalar_mesa $URL_ATUAL
+        instalar_mesa $URL_XCLIPSE
         ;;
     2)
-        instalar_mesa $URL_RECENTE
+        instalar_mesa $URL_BE
         ;;
     3)
         instalar_mesa $URL_MALI
@@ -44,9 +43,9 @@ case $escolha in
         exit 0
         ;;
     *)
-        echo "Escolha inválida! Tente novamente."
+        echo "Escolha inválida!"
         ;;
 esac
 
 clear
-echo "O Mesa foi atualizado!"
+echo "O Mesa foi atualizado"
