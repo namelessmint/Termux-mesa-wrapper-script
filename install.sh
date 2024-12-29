9f07ee1f3941f9c929471759039c0526215a477c
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2024-12-29 20:49:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,8 @@ dpkg -i ./.install/*.deb
 wget https://github.com/alexvorxx/hangover-termux/releases/download/9.22/wine_hangover_9.22_bionic_build_patched.tar.xz
 mv wine_hangover_* wine_hangover.tar.xz
 tar -xf wine_hangover.tar.xz
+WINEPREFIX=~/.wine64
+winetricks dxvk vcrun2019 dotnet40
 
 wget https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/Arquivos3/script.zip
 unzip -o script.zip -d /sdcard/Download/DXVK-3DTESTE/
