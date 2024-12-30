64797a5da1ee78092368236f561c1204f09fe7c2
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2024-12-30 17:55:39 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,17 +11,14 @@ pkg install tur-repo x11-repo -y
 sed -i '1s/$/ tur-multilib/' /data/data/com.termux/files/usr/etc/apt/sources.list.d/tur.list
 pkg update && pkg upgrade -y
 pkg install hangover winetricks cabextract termux-x11-* termux-api freetype git gnutls libandroid-shmem-static libx11 xorgproto libdrm libpixman libxfixes libjpeg-turbo mesa-demos osmesa mesa pulseaudio termux-x11-nightly vulkan-tools xtrans libxxf86vm xorg-xrandr xorg-font-util xorg-util-macros libxfont2 libxkbfile libpciaccess xcb-util-renderutil xcb-util-image xcb-util-keysyms xcb-util-wm xorg-xkbcomp xkeyboard-config libxdamage libxinerama libxshmfence wget -y
-mkdir .install .update .script /sdcard/Download/DXVK-3DTESTE/
+mkdir .install .update .script
 clear
 
 echo "Baixando e configurando scripts Adicionais"
 wget -q https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/test.sh
-wget -q https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/start.sh
 mv test.sh .script/.test.sh
 echo "alias 3d-teste='~/.script/.test.sh'" >> .bashrc
 echo "alias mesa-update='bash <(curl -s https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/update.sh)'" >> .bashrc
-mv start.sh .script/.start.sh
-echo "alias pc-emu='~/.script/.start.sh'" >> .bashrc
 chmod +x .script/.*.sh
 
 echo "Instalando o Mesa"
@@ -29,23 +26,10 @@ wget -q https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/down
 dpkg -i ./.install/*.deb
 clear
 
-echo "Baixando e configurando a Wine Hangover"
-wget -q https://github.com/alexvorxx/hangover-termux/releases/download/9.22/wine_hangover_9.22_bionic_build_patched.tar.xz
-mv wine_hangover_* wine_hangover.tar.xz
-tar -xf wine_hangover.tar.xz
-WINEPREFIX=~/.wine64
-winetricks dxvk vcrun2019 dotnet40
-clear
-
-echo "Baixando Arquivos Adicionais"
-wget -q https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/Arquivos3/script.zip
-echo "Extraindo Arquivos para Downloads"
-unzip -oq script.zip -d /sdcard/Download/DXVK-3DTESTE/
-
 echo "Limpamdo arquivos temporarios"
-rm -r .install/ wine_hangover.tar.xz  script.zip
+rm -r .install/
 
-echo "printf '\n Use o comando mesa-update para atualizar o Mesa e pc-emu para iniciar o Wine. \n'" >> .bashrc
+echo "printf '\n Use o comando mesa-update para atualizar o Mesa. \n'" >> .bashrc
 
 ln -s /sdcard/Download/ Downloads
 
