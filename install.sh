b8bec66c4dc5878b73bad9843bba9245872114d9
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2024-12-30 11:24:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,8 @@ pkg install tur-repo x11-repo -y
 sed -i '1s/$/ tur-multilib/' /data/data/com.termux/files/usr/etc/apt/sources.list.d/tur.list
 pkg update && pkg upgrade -y
 pkg install hangover winetricks cabextract termux-x11-* termux-api freetype git gnutls libandroid-shmem-static libx11 xorgproto libdrm libpixman libxfixes libjpeg-turbo mesa-demos osmesa mesa pulseaudio termux-x11-nightly vulkan-tools xtrans libxxf86vm xorg-xrandr xorg-font-util xorg-util-macros libxfont2 libxkbfile libpciaccess xcb-util-renderutil xcb-util-image xcb-util-keysyms xcb-util-wm xorg-xkbcomp xkeyboard-config libxdamage libxinerama libxshmfence wget -y
-clear
-
 mkdir .install .update .script /sdcard/Download/DXVK-3DTESTE/
+clear
 
 echo "Baixando e configurando scripts Adicionais"
 wget -q https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/test.sh
@@ -28,6 +27,7 @@ chmod +x .script/.*.sh
 echo "Instalando o Mesa"
 wget -q https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/16%2F12/mesaup.deb -O .install/mesa.deb
 dpkg -i ./.install/*.deb
+clear
 
 echo "Baixando e configurando a Wine Hangover"
 wget -q https://github.com/alexvorxx/hangover-termux/releases/download/9.22/wine_hangover_9.22_bionic_build_patched.tar.xz
