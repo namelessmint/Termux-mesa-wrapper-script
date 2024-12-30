ebc8360171c2d803adea16005ccd0e391ca64ab7
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2024-12-30 18:08:26 -0300

    Update winehangover.sh

--- winehangover.sh
+++ winehangover.sh
@@ -4,20 +4,20 @@ clear
 
 pkg install hangover winetricks cabextract
 mkdir /sdcard/Download/DXVK-3DTESTE/
+clear
 
 echo "Baixando e configurando a Wine Hangover"
 wget -q https://github.com/alexvorxx/hangover-termux/releases/download/9.22/wine_hangover_9.22_bionic_build_patched.tar.xz
 mv wine_hangover_* wine_hangover.tar.xz
 tar -xf wine_hangover.tar.xz
 WINEPREFIX=~/.wine64
-winetricks dxvk vcrun2019 dotnet40
-clear
+winetricks dxvk vcrun2019 dotnet40 -y
 
 echo "Baixando Arquivos Adicionais"
 wget -q https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/Arquivos3/script.zip
 echo "Extraindo Arquivos para Downloads"
 unzip -oq script.zip -d /sdcard/Download/DXVK-3DTESTE/
-
+clear
 
 echo "Limpamdo arquivos temporarios"
 rm  wine_hangover.tar.xz  script.zip
@@ -28,6 +28,6 @@ mv start.sh .script/.start.sh
 echo "alias pc-emu='~/.script/.start.sh'" >> .bashrc
 chmod +x .script/.start.sh
 
-echo "printf '\n Use o comando pc-emu para iniciar o Wine. \n'" >> .bashrc
+echo "printf 'Use o comando pc-emu para iniciar o Wine. \n'" >> .bashrc
 
 clear
