6d49183474da69b79fed0f0f7067eaba0c2df3d9
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2025-01-01 18:11:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ pkg update && pkg upgrade -y
 clear
 echo "Instalando Pacotes adicionais..."
 pkg install tur-repo x11-repo -y &> /dev/null
-sed -i '1s/$/ tur-multilib/' /data/data/com.termux/files/usr/etc/apt/sources.list.d/tur.list
+sed -i '1s/$/ tur-multilib/' /data/data/com.termux/files/usr/etc/apt/sources.list.d/tur.list &> /dev/null
 pkg update && pkg upgrade -y &> /dev/null
 pkg install termux-x11-* termux-api freetype git gnutls libandroid-shmem-static libx11 xorgproto libdrm libpixman libxfixes libjpeg-turbo mesa-demos osmesa mesa pulseaudio termux-x11-nightly vulkan-tools xtrans libxxf86vm xorg-xrandr xorg-font-util xorg-util-macros libxfont2 libxkbfile libpciaccess xcb-util-renderutil xcb-util-image xcb-util-keysyms xcb-util-wm xorg-xkbcomp xkeyboard-config libxdamage libxinerama libxshmfence wget -y &> /dev/null
 mkdir .install .update .script
