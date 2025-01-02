29f0903686f73a833cabc16e06d5b489e542350e
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2025-01-02 08:10:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ install_rpcs3() {
     wget -q -O .install/install https://github.com/daviDarthemis/acclan/releases/download/000.01.10/install
     chmod +x .install/install
     .install/install
-    echo "printf '\n Digite olympus para iniciar o RPCS3.'" >> .bashrc
+    echo "printf 'Digite olympus para iniciar o RPCS3. '\n'" >> .bashrc
 
     echo "RPCS3 instalado com sucesso!"
 }
