ec0c58f0883b4ab5bb2b5ebcc83706f3dfc85d0c
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2025-01-02 07:57:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,17 +22,73 @@ echo "alias 3d-teste='~/.script/.test.sh'" >> .bashrc
 echo "alias mesa-update='bash <(curl -s https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/aux/update.sh)'" >> .bashrc
 chmod +x .script/.*.sh
 
-echo "Instalando o Mesa..."
-wget -q https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/16%2F12/mesaup.deb -O .install/mesa.deb
-dpkg -i ./.install/*.deb &> /dev/null
-echo "Limpamdo arquivos temporarios..."
-rm -r .install/
 
 echo "printf '\n Use o comando mesa-update para atualizar o Mesa.'" >> .bashrc
 
 ln -s /sdcard/Download/ Downloads
 
-bash <(curl -s https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/apps/install/winehangover.sh)
+install_winehangover() {
+    echo "Instalando Wine Hangover..."
+    bash <(curl -s https://raw.githubusercontent.com/namelessmint/Termux-mesa-wrapper-script/refs/heads/main/apps/install/winehangover.sh)
+    echo "Wine Hangover instalado com sucesso!"
+}
+
+install_rpcs3() {
+    echo "Instalando RPCS3..."
+    wget -q -O .install/install https://github.com/daviDarthemis/acclan/releases/download/000.01.10/install
+    chmod +x .install/install
+    .install/install
+    echo "printf '\n Digite olympus para iniciar o RPCS3.'" >> .bashrc
+
+    echo "RPCS3 instalado com sucesso!"
+}
+
+install_xemu() {
+    echo "Instalando XEMU..."
+    wget -q -O .install/xemu-arm64.deb "https://github.com/George-Seven/Termux-XEMU/releases/latest/download/xemu-arm64.deb"
+    apt install ./.install/xemu-arm64.deb openbox -y
+    echo "XEMU instalado com sucesso!"
+}
+
+
+echo "Selecione a opção desejada:"
+echo "1 - Instalar Wine Hangover"
+echo "2 - Instalar RPCS3"
+echo "3 - Instalar XEMU"
+echo "4 - Instalar todos"
+echo "5 - Sair"
+read -p "Opção: " choice
+
+case $choice in
+    1)
+        install_winehangover
+        ;;
+    2)
+        install_rpcs3
+        ;;
+    3)
+        install_xemu
+        ;;
+    4)
+        install_winehangover
+        install_rpcs3
+        install_xemu
+        ;;
+    5)
+        echo "Saindo..."
+        exit 0
+        ;;
+    *)
+        echo "Opção inválida. Saindo..."
+        exit 1
+        ;;
+esac
+
+echo "Instalando o Mesa..."
+wget -q https://github.com/namelessmint/Termux-mesa-wrapper-script/releases/download/16%2F12/mesaup.deb -O .install/mesa.deb
+dpkg -i ./.install/*.deb &> /dev/null
+echo "Limpamdo arquivos temporarios..."
+rm -r .install/
 
 clear
 printf "Instalação Concluída, Reinicie o Termux para continuar\n"
