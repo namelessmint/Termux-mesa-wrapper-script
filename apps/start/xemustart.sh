48de0e73fec0cfb28e867b9134dbe77f3c151413
iago felipe da silva <63817034+namelessmint@users.noreply.github.com>
2025-01-02 09:05:11 -0300

    Add files via upload

---
+++ apps/start/xemustart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+termux-x11 :0 &>/dev/null &
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1 &>/dev/null
+export DISPLAY=:0
+xemu
